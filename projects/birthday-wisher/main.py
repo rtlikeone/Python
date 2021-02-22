1453715a2341b6685a22ef8fad83e344ceeae5b1
rtlikeone <info@wslm.nl>
2021-02-22 15:43:43 +0100

    refactor

--- projects/birthday-wisher/main.py
+++ projects/birthday-wisher/main.py
@@ -4,42 +4,33 @@ import random
 import smtplib
 
 
-email = "***"
-password = "***"
-to_addr = "***"
+email = "romsontesting@gmail.com"
+password = "Welcome2021*"
+to_addr = "info@vbel.nl"
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day = now.day
-today = f"{day}-{month}-{year}"
+today = (now.month, now.day)
 
 # Add names to birthdays.csv
 # with open("birthdays.csv", "a") as file:
-#     file.write("Romero, romsontesting@gmail.com, 1982, 12, 09\n")
+#     file.write("Name, name@gmail.com, 2002, 01, 01\n")
 
-# Create DataFrame
-data = pandas.read_csv("birthdays.csv")
+data_csv = pandas.read_csv("birthdays.csv")
+birthdays = {(data.month, data.day): data for (index, data) in data_csv.iterrows()}
 
-# Open letter templates
-with open(f"letter_templates/letter_{random.randint(1, 3)}.txt", mode="r") as source_letter:
-    source_contents = source_letter.read()
+if today in birthdays:
+    person = birthdays[today]
 
-    # Check for date, name etc..
-    for (index, person) in data.iterrows():
-        if f"{person['day']}-{person['month']}-{person['year']}" == "9-12-1982":
-            name = person['name']
-            print(name)
-            # Replace name
-            letter_to_send = source_contents.replace("[NAME]", name)
-            mssg = f"Subject: Happy Birhtday!\n\n{letter_to_send}"
+    with open(f"letter_templates/letter_{random.randint(1, 3)}.txt", mode="r") as source_letter:
+        source_contents = source_letter.read()
+        letter_to_send = source_contents.replace("[NAME]", person["name"])
+        mssg = f"Subject: Happy Birhtday!\n\n{letter_to_send}"
 
-            # Send email
-            with smtplib.SMTP("smtp.gmail.com") as connection:
-                connection.starttls()
-                connection.login(user=email, password=password)
-                connection.sendmail(
-                    from_addr="Birthday service",
-                    to_addrs=to_addr,
-                    msg=mssg
-                )
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(
+            from_addr="Birthday service",
+            to_addrs=to_addr,
+            msg=mssg
+        )
